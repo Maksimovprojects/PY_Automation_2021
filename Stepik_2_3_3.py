1bf3dbd9b6492fc7e2668ff068fc6880c0800442
Maksim Maksimov <maximebayer@gmail.com>
2021-12-01 00:21:52 -0500

    add accept() method for interact pop-up window + comments

--- Stepik_2_3_3.py
+++ Stepik_2_3_3.py
@@ -3,26 +3,31 @@ import math
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
-def calc(x):
-  return str(math.log(abs(12 * math.sin(int(x)))))
-
 try:
-  browser = webdriver.Chrome()
-  browser.get("http://suninjuly.github.io/alert_accept.html")
-  button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
-  button.click()
-  confirm = browser.switch_to.alert #switch to pop-up Alert window
-  confirm.accept()
-  x = browser.find_element(By.ID, "input_value").text
-  y = calc(x)
-  input_fieid = browser.find_element(By.ID, "answer")
-  input_fieid.send_keys(y)
-  button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
-  button.click()
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/redirect_accept.html")
+    button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button.click()
+    ListTabs = browser.window_handles  # return list of names of all opened tabs in browser
+    # Debug actions
+    print(type(ListTabs))  # print on console type of variable
+    print(ListTabs)  # print on console each element(names of tabs) in list
+
+    SecondTab = browser.window_handles[1]  # assign "new_window" variable the name of the second
+    # element in list of opened tabs
+    browser.switch_to.window(SecondTab)  # Switch to window/Tab having tab name assigned to SecondTab
+
+    def calc(num):
+        result = str(math.log(abs(12 * math.sin(int(num)))))
+        return result
+
+    x = browser.find_element(By.ID, "input_value").text
+    y = calc(x)
+    input_fieid = browser.find_element(By.ID, "answer")
+    input_fieid.send_keys(y)
+    button = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button.click()
 
 finally:
-  time.sleep(6)
-  browser.quit()
-
-
+    time.sleep(3)
+    browser.quit()
