7b5ac3f821d98e0a5a788b6a587a0dbd025823cb
Maksim Maksimov <maximebayer@gmail.com>
2021-12-05 12:23:18 -0500

    add unittest approach in testing 2 registering pages

---
+++ Stepik_3_2_2.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import unittest
+
+class TestStepikWebSite(unittest.TestCase):
+
+    def test_RegisterForm1(self):
+        browser = webdriver.Chrome()
+        browser.get("http://suninjuly.github.io/registration1.html")
+        InputFirstName = browser.find_element(By.XPATH, '//input[@placeholder="Input your first name"]')
+        InputFirstName.send_keys("Ivan")
+        InputLastName = browser.find_element(By.XPATH, '//input[@placeholder="Input your last name"]')
+        InputLastName.send_keys("Ivanov")
+        InputEmailData = browser.find_element(By.XPATH, '//input[@placeholder="Input your email"]')
+        InputEmailData.send_keys("12345@12345.com")
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
+        browser.quit()
+
+    def test_RegisterForm2(self):
+        browser = webdriver.Chrome()
+        browser.get("http://suninjuly.github.io/registration1.html")
+        InputFirstName = browser.find_element(By.XPATH, '//input[@placeholder="Input your first name"]')
+        InputFirstName.send_keys("Ivan")
+        InputEmailData = browser.find_element(By.XPATH, '//input[@placeholder="Input your email"]')
+        InputEmailData.send_keys("12345@12345.com")
+        browser.find_element(By.CSS_SELECTOR, "button.btn").click()
+        welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+        welcome_text = welcome_text_elt.text
+        self.assertEqual(welcome_text, "Congratulations! You have successfully registered!")
+        browser.quit()
+
+if __name__ == "__main__":
+    unittest.main()
