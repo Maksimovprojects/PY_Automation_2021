e3948a83cc75118899dfd9bbf191c707a2f977c3
Maksim Maksimov <maximebayer@gmail.com>
2021-12-17 21:28:17 -0500

    add desctiption about test markers into pytest.ini and in file itself

--- Stepik_3_5_1_MarkingTests.py
+++ Stepik_3_5_1_MarkingTests.py
@@ -12,15 +12,15 @@ def browser():
     print("\nquit browser...")
     browser.quit()
 
-
+@pytest.mark.TestMainPage1 # marking for executing tests into class (check marker into file pytest.ini)
 class TestMainPage1():
 
-    @pytest.mark.smoke
+    @pytest.mark.smoke # marking (smoke) for executing test into method (check marker into file pytest.ini)
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-    @pytest.mark.regression
+    @pytest.mark.regression # marking (regression) for executing test into method (check marker into file pytest.ini)
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
