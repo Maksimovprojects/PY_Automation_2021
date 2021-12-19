89483a3ec850575fe2f2503b7f4bb6f19aea34b7
Maksim Maksimov <maximebayer@gmail.com>
2021-12-18 20:08:26 -0500

    add 'skip' marker to test and select/diselect markers

---
+++ Stepik_3_5_3_DiselectionTests.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test...")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser...")
+    browser.quit()
+
+@pytest.mark.TestMainPage1 # marking for executing tests into class (check marker into file pytest.ini)
+class TestMainPage1():
+
+    @pytest.mark.smoke # marking (smoke) for executing test into method (check marker into file pytest.ini)
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    @pytest.mark.skip # marking "skip" for skipping particular test no need add that info tp pytest.ini
+    @pytest.mark.regression
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
